306c7558bd87253feb5d166df009ff3b11fc007c
Seokhyeon Jang <coolman555@me.com>
2021-06-09 06:14:51 +0900

    feat: menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,21 @@
 <template>
   <v-app>
-    <v-app-bar
-      rounded
-      dense
-      dark
-      class="ml-5 mr-5 mt-3"
-      style="max-height: 45px"
-    >
-      test
+    <!-- Header Start -->
+    <v-app-bar flat class="menu">
+      <div>
+        <h3>Seokhyun Jang</h3>
+      </div>
+      <div>
+        <ul class="menu-wrap">
+          <li>홈</li>
+          <li>보유기술</li>
+          <li>대표 프로젝트</li>
+          <li>철학과 목표</li>
+          <li>블로그</li>
+        </ul>
+      </div>
     </v-app-bar>
+    <!-- Header End -->
 
     <!-- Background IMG Start -->
     <div class="bg-icon">
@@ -18,14 +25,19 @@
     </div>
     <!-- Background IMG End -->
 
+    <!-- Main Start -->
     <v-main>
       <v-container fluid>
         <nuxt />
       </v-container>
     </v-main>
-    <v-footer app>
+    <!-- Main End -->
+
+    <!-- Footer Start -->
+    <v-footer>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
+    <!-- Footer End -->
   </v-app>
 </template>
 
@@ -43,5 +55,41 @@
 }
 .bg-icon > div {
   margin: auto;
+}
+
+/* Menu Design */
+.menu {
+  width: 100%;
+  max-width: 1200px;
+  max-height: 50px;
+  margin: auto;
+}
+.menu-wrap {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+.menu-wrap > li {
+  float: left;
+  margin: 0;
+  margin-right: 1.2rem;
+  padding: 0;
+  border: 0;
+  color: #333;
+}
+.v-toolbar__content {
+  border-radius: 3px;
+  border-left: 10px solid #333;
+  display: block !important;
+}
+header {
+  margin-top: 13px !important;
+}
+.theme--light.v-app-bar.v-toolbar.v-sheet {
+  background: transparent;
+}
+.v-application ul,
+.v-application ol {
+  padding-left: 0;
 }
 </style>
