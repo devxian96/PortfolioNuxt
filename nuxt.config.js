990f8cc7e54991c7d6049681665ac2e15e0ee512
Seokhyeon Jang <coolman555@me.com>
2021-06-09 09:51:12 +0900

    fix: cd deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,9 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/PortfolioNuxt/',
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
