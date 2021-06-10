3aafcb082cf227ce629613f6272f0c59c951018a
devxian96 <devxian96@users.noreply.github.com>
2021-06-10 08:14:56 +0000

    deploy: 6a46b96e156a551e84275e7cf70d8fcd490a8c3d

---
+++ _nuxt/280dca3.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{407:function(e,n,t){"use strict";t.r(n);var l=t(79),component=Object(l.a)({},(function(){var e=this,n=e.$createElement;return(e._self._c||n)("div",[e._v("test")])}),[],!1,null,null,null);n.default=component.exports}}]);
---
+++ _nuxt/static/1623312894/skill/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/skill", {data:[{}],fetch:{},mutations:void 0});
