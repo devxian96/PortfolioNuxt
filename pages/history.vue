0b9479820a647b9972050543392d9ff212927b0e
Seokhyeon Jang <coolman555@me.com>
2021-06-11 16:02:47 +0900

    feat: history

--- pages/history.vue
+++ pages/history.vue
@@ -1,3 +1,261 @@
 <template>
-  <div>역사</div>
+  <v-row>
+    <!-- 2021 -->
+    <div class="box mb-5 pt-2 pb-2">
+      <h3>2021</h3>
+      <hr />
+      <p>한림대학교 복학</p>
+      <p>
+        React, Redux, Nextjs, Typescript, Material UI, Flask, Orion, influxDB,
+        Github CICD Action, Thymeleaf, ejs, Swift, LSTM + Annomaly Detection ML
+        추가 학습, FireBase, Redis
+      </p>
+      <p>쇼핑몰 개발(FireBase, Nuxtjs)</p>
+      <p>
+        소프트젠(2021.04 ~ 2021.05, 2개월, 연구원) - AI 관제 시스템 개발 (React,
+        Redux, Flask, ML, Redis, Express, LSTM + Annomaly Detection등)
+      </p>
+      <p>
+        2020년에 개발하였던 라이센스 시스템에서 사용한 자체개발 프레임워크
+        <a href="https://github.com/devxian96/phpExpress" target="_blank"
+          >phpExpress 오픈소스</a
+        >화
+      </p>
+      <p>
+        VueJS에 있는 글로벌 컴포넌트 기능 React에서도 비슷한 기능으로 라이브러리
+        구현(<a
+          href="https://github.com/devxian96/react-global-components"
+          target="_blank"
+          >react-global-components</a
+        >)
+      </p>
+      <p>
+        아주대학교 멘토(2021.02 ~ 2021.06) -
+        <a href="https://github.com/neomorphism/neomo" target="_blank"
+          >뉴모피즘 디자인 프레임워크</a
+        >
+        개발 지도및 교육, 컨트리뷰트
+      </p>
+      <p>
+        Zum-core SPA 프레임워크 개발(Typescript) - 버츄얼돔이 없는 기본 형태
+        (<small
+          >이벤트 객채를 사용하여 버츄얼돔을 대체할 새로운 개념의 프로그래시브
+          웹 프레임워크 오픈소스 Rapid를 준비하고 있습니다. 2021년 8월쯤 공개
+          예정</small
+        >)
+      </p>
+      <p>
+        <a href="https://devxian96.github.io/portfolionuxt/" target="_blank"
+          >포트폴리오 웹사이트</a
+        >
+        개발(2021.06, 1달, Nuxtjs, 글래스모피즘)
+      </p>
+      <p>
+        환경부 Smart Air Screen 수정및 개발(3인, 2021.3 , 2021.06, 2달, Spring
+        Boot, Thymeleaf)
+      </p>
+    </div>
+
+    <!-- 2020 -->
+    <div class="box mb-5 pt-2 pb-2">
+      <h3>2020</h3>
+      <hr />
+      <p>한림대학교 휴학</p>
+      <p>
+        <a href="https://heybirdie.co.kr" target="_blank">헤이버디</a> 나와 맞는
+        골프 찾기 스타트업(2019.12 ~ 2020.08, 팀원) - 헤이버디(아래 기술스택
+        전체 공부후 사용)
+      </p>
+      <p>
+        Nuxt.js, Forever, PM2, AWS EC2, AWS RDB, AWS Route53, AWS lightsail,
+        Notion, Zeplin, Babel, pollyfill, Webpack, Vuex, nodeJS, Express,
+        Sequelize, Github Husky, CICD, JEST, vuetify, AMP, PWA, NativeScript,
+        Eslint, EsHint, Commit Lint, Style Lint, Prettier, i18n, Electron
+      </p>
+      <p>
+        엔텀 오픈소스 빌드 및 라이센스 관리도구 개발(3인, 2020.09 ~ 2020.11,
+        3개월, Electron, C#, NuxtJS)
+      </p>
+      <p>라이센스 시스탬 개발(2인, 2020.12 ~ 2021.01, 2개월, PHP7, Nuxtjs)</p>
+      <p>findURL 개발(Python3)</p>
+    </div>
+
+    <!-- 2019 -->
+    <div class="box mb-5 pt-2 pb-2">
+      <h3>2019</h3>
+      <hr />
+      <p>한림대학교 빅데이터학과 편입</p>
+      <p>
+        Keras, Maltplot, Srping MVC, myBatis, JPA(JSTL), 전자정부프레임워크,
+        Tomcat, zeus, Spring Boot, VueJS, npm, yarn, Javascript ES6-8, premiere
+        PRO, R, Slack
+      </p>
+      <p>
+        프리랜서 활동.
+        <a href="https://www.asiantipster.com/" target="_blank">아시안 팁스터</a
+        >및 여러개의 스포츠 관련 홈페이지, 쇼핑몰, 커뮤니티, 인스타 페이스북용
+        이미지 편집등 진행, 공장 수율 검사 프로그램 Z-view 후킹하여 보고서 자동
+        작성, 공장 모니터링 대시보드 웹 서비스용 구글 익스탠션 개발 제공,
+        nBand와 트위터 인스타 마케팅 프로그램 개발
+      </p>
+      <p>M2F1 설문조사 시스템 개발(3인팀 교내 프로젝트, PHP7, MariaDB)</p>
+      <p>
+        (주) 피타그래프 연구원(2019.08 ~ 2019.09, 2달) - 서울 상권분석
+        설문조사시스템 개발(전자정부프레임워크, VueJS)
+      </p>
+      <p>
+        캠월드 영상 자동 수집및 링크사이트 개발및 운영(2인팀, 1년, PHP7,
+        MariaDB, Python3)
+      </p>
+      <p>
+        Krafton 배틀그라운드 게임핵 개발 경로 취약점 제보, 하였으나 제보한 계정
+        정지됨.(현 시점 배틀그라운드 핵이 제보한 내용대로 개발되어지고 우회되고
+        있음)
+      </p>
+      <p>
+        한국교통공사 주민등록번호를 통해 운전면허 갱신 기간 조회 서비스를 통해
+        개인정보 유출 취약점 제보, 그러나 그런 취약점 없다는 답변과 함께 당일
+        취약한 부분 보안 서비스 설치를 통해 취약점 패치됨.
+      </p>
+      <p>
+        정보보안에 큰 관심을 가지고 있었으나 4차례의 버그 제보 후 큰 회의감을
+        느껴 관심을 접고 Full-Stack, Front-End 개발자의 길을 준비함 함
+      </p>
+    </div>
+
+    <!-- 2018 -->
+    <div class="box mb-5 pt-2 pb-2">
+      <h3>2018</h3>
+      <hr />
+      <p>군 전역</p>
+      <p>BackMusic 백그라운드 음원 재생기 개발(Autoit)</p>
+      <p>원페이지 (SPA) 서비스 시작(6인, 4개월, PHP7, MySQL)</p>
+    </div>
+
+    <!-- 2017 -->
+    <div class="box mb-5 pt-2 pb-2">
+      <h3>2017</h3>
+      <hr />
+      <p>군 입대</p>
+      <p>Jupyter notebook, TensorFlow, python3</p>
+      <p>랜덤 함수 역산 강화학습 인공신경망 프로그램(Python3, TF)</p>
+    </div>
+
+    <!-- 2016 -->
+    <div class="box mb-5 pt-2 pb-2">
+      <h3>2016</h3>
+      <hr />
+      <p>HTML5, CSS3, PHP7, JQuery, Java</p>
+      <p>여주시 UCC 영상 대회 대상</p>
+      <p>P2P 바이어 매칭 사이트 개발(4인팀 교내 프로젝트)</p>
+      <p>
+        프리랜서 활동. inN방문기, Youtube 마케팅 프로그램, 오토바이 중고 거래
+        사이트, 쇼핑몰 모델 사진 수정 500여장, Youtube 크리에이터 영상 외주등
+        작업
+      </p>
+      <p>영상 스트리밍 사이트 개발 운영(1년)</p>
+      <p>
+        Nexon 서든어택 키보드 마우스 제어및 원격 침투 보안취약점 제보,
+        버그바운티를 진행하지 않는다며 반려 (2021년 4월 기준 아직도 해당 취약점
+        존재, 시중에 나온 에임봇, 에임핵, 오토에임, 텔포핵등이 해당 취약점을
+        이용중)
+      </p>
+      <p>
+        Riot 리그오브레전드 메모리 접근및 내부 함수 접근 취약점 제보,
+        버그바운티를 진행하지 않는다며 반려 (2021년 4월 기준 아직도 해당 취약점
+        존재, 시중에 나온
+        <a href="https://hanbot.gg/" target="_blank">한봇</a>을 포함한 대부분의
+        게임핵이 해당 취약점을 이용중)
+      </p>
+      <p>개인 네트워크 방화벽 개발(Python3, Windows Firewall)</p>
+      <p>에이원네트웍스 인턴(2016.08 ~ 2016.09, 2달) - 사무보조 마케팅</p>
+      <P
+        >Teardown, DRDOS, DOS, DDOS등 여러개의 네트워크 공격을 할 수 있는 도구
+        개발(Autoit, Python, C)</P
+      >
+    </div>
+
+    <!-- 2015 -->
+    <div class="box mb-5 pt-2 pb-2">
+      <h3>2015</h3>
+      <hr />
+      <p>여주대 컴퓨터정보과 입학</p>
+      <p>
+        Oracle, PostgreSQL, H/W, Linux Server, SVN, FTP Server, Email Server,
+        Dns Server, Window Server, Bootstrap
+      </p>
+      <p>PHP5, MySQL, MariaDB, Javascript(ES5)</p>
+      <p>
+        그누보드 아미나 기반 쇼핑몰
+        <a href="http://kingbully.co.kr" target="_blank">킹불리 개발</a> (첫
+        웹개발 시작)
+      </p>
+      <p>
+        프리랜서로 활동 시작. 코세퀸, 핫몽, 골드월드, 참평안요양원및 교회,
+        커뮤니티, 결제모듈, 유지보수 및 오류 버그 수정. Vlog, 대학생 영상평가,
+        취업 영상, 포토샵 작업등. XE 보드, 워드프레스, 그누보드
+      </p>
+      <p>lisp, prolog</p>
+      <p>키로거, RDP 개발(Autoit)</p>
+    </div>
+
+    <!-- 2014 -->
+    <div class="box mb-5 pt-2 pb-2">
+      <h3>2014</h3>
+      <hr />
+      <p>inN방문기 (Autoit)</p>
+      <p>
+        UNIX, Router IOS OS(CCNA, CCNP 준비), TCP/IP 네트워크, 리버싱,
+        Metaploit, Pentesting등 보안 분야 학습. (ITBANK)
+      </p>
+    </div>
+
+    <!-- 2013 -->
+    <div class="box mb-5 pt-2 pb-2">
+      <h3>2013</h3>
+      <hr />
+      <p>월드비젼 교실에서 찾은 희망 UCC 2회 수상</p>
+      <p>게임 캐릭터 제어 프로그램(C#)</p>
+      <p>랜덤 함수 역산 프로그램(Autoit)</p>
+      <p>메모장 프로그램(Autoit)</p>
+      <p>동영상 재생 프로그램(Autoit)</p>
+      <p>
+        한국생활건강 계약직(2013.08, 1달) - 연관검색어 프로그램(Autoit) 개발
+      </p>
+      <p>실시간 검색어, 연관 검색어, 상위노출, 바이럴마케팅등 학습</p>
+    </div>
+
+    <!-- 2012 -->
+    <div class="box mb-5 pt-2 pb-2">
+      <h3>2012</h3>
+      <hr />
+      <p>월드비젼 교실에서 찾은 희망 UCC 수상</p>
+      <p>C#, Git(Github), HTML, CSS</p>
+    </div>
+
+    <!-- 2011 -->
+    <div class="box mb-5 pt-2 pb-2">
+      <h3>2011</h3>
+      <hr />
+      <p>가운중학교 1회 졸업생 전자 졸업 엘범 제작</p>
+      <p>Vegas PRO, Adobe Effect, Cinema 4D</p>
+      <p>Python2</p>
+    </div>
+
+    <!-- 2010 -->
+    <div class="box mb-5 pt-2 pb-2">
+      <h3>2010</h3>
+      <hr />
+      <p>Antivirus 개발(Autoit)</p>
+      <p>bash, linux, 해킹과 보안</p>
+    </div>
+
+    <!-- 2009 -->
+    <div class="box mb-5 pt-2 pb-2">
+      <h3>2009</h3>
+      <hr />
+      <p>FireCleaner 임시파일 삭제프로그램 개발(Autoit, C)</p>
+      <p>C, C++, Autoit</p>
+    </div>
+  </v-row>
 </template>
