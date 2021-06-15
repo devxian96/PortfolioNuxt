23bc74894d23fae8af04170b800b58442c58a99f
devxian96 <devxian96@users.noreply.github.com>
2021-06-15 00:12:29 +0000

    deploy: 6f2427e787a5584baae71f65d635f0d35c3fc040

---
+++ _nuxt/static/1623715946/skill/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FPortfolioNuxt\u002F_nuxt\u002Fstatic\u002F1623715946",layout:"default",error:a,serverRendered:true,routePath:"\u002Fskill",config:{_app:{basePath:"\u002FPortfolioNuxt\u002F",assetsPath:"\u002FPortfolioNuxt\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9fd374b0"}}}}(null));
