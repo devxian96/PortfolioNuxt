dec287782f56fad4f2018d89c85cd945dcf2dceb
devxian96 <devxian96@users.noreply.github.com>
2021-06-17 06:55:06 +0000

    deploy: 13343f55f124226d97fb3636422f175fdc22875c

---
+++ _nuxt/static/1623912900/blog/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FPortfolioNuxt\u002F_nuxt\u002Fstatic\u002F1623912900",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog",config:{_app:{basePath:"\u002FPortfolioNuxt\u002F",assetsPath:"\u002FPortfolioNuxt\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6ddfb883"}}}}(null));
