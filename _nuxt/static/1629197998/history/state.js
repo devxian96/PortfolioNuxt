18864b5be32d144576a351f9649f3e2d744c63da
devxian96 <devxian96@users.noreply.github.com>
2021-08-17 11:00:01 +0000

    deploy: 1c808da0192d33eebe3f2952234e8099a3f26930

---
+++ _nuxt/static/1629197998/history/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FPortfolioNuxt\u002F_nuxt\u002Fstatic\u002F1629197998",layout:"default",error:a,serverRendered:true,routePath:"\u002Fhistory",config:{_app:{basePath:"\u002FPortfolioNuxt\u002F",assetsPath:"\u002FPortfolioNuxt\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c7e25c50"}}}}(null));
---
+++ _nuxt/static/1629197998/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fblog","\u002Fhistory","\u002Fskill","\u002Fproject","\u002Fblog\u002Fintro"]})
