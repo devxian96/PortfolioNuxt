4ae5b326a1f13d012ae21c48f586079eada52ffe
DevXian <coolman555@me.com>
2022-01-25 01:51:43 +0900

    test: test case code base

--- jest.config.js
+++ jest.config.js
@@ -3,17 +3,28 @@ module.exports = {
     '^@/(.*)$': '<rootDir>/$1',
     '^~/(.*)$': '<rootDir>/$1',
     '^vue$': 'vue/dist/vue.common.js',
-    '@/(.*)$': '<rootDir>/src/$1',
   },
   moduleFileExtensions: ['ts', 'js', 'vue', 'json'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
     '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest',
+    '^.+\\.vue$': 'vue-jest',
   },
   collectCoverage: true,
+  testRegex: '\\.test\\.ts$',
   collectCoverageFrom: [
-    '<rootDir>/components/**/*.vue',
-    '<rootDir>/pages/**/*.vue',
+    'components/**/*.vue',
+    'layouts/**/*.vue',
+    'pages/**/*.vue',
+    'lib/**/*.ts',
+    'plugins/**/*.ts',
+    'store/**/*.ts',
   ],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  moduleDirectories: ['node_modules', '.'],
+  globals: {
+    'ts-jest': {
+      diagnostics: true,
+    },
+  },
 }
--- test/Index.test.ts
+++ test/Index.test.ts
@@ -1,10 +1,16 @@
+/**
+ * @jest-environment jsdom
+ */
 import Vue from 'vue'
 import Vuetify from 'vuetify'
+import { mount } from '@vue/test-utils'
+import Index from '@/pages/history.vue'
 
 Vue.use(Vuetify)
 
 describe('Index Page', () => {
   test('is a Vue instance', () => {
-    expect(true).toBeTruthy()
+    const wrapper = mount(Index)
+    expect(wrapper.vm).toBeTruthy()
   })
 })
