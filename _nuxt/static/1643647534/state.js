19b85418ea407b5d178e0dfcf934559b3011d07c
devxian96 <devxian96@users.noreply.github.com>
2025-11-30 06:54:22 +0000

    deploy: 064f8d2dfaf8365682e44c9817a98bd72dba2897

--- _nuxt/static/1643647534/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FPortfolioNuxt\u002F_nuxt\u002Fstatic\u002F1643647534",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FPortfolioNuxt\u002F",assetsPath:"\u002FPortfolioNuxt\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c9b7e12d"}}}}(null));
